b6edcbf93b2a5b201834f97477ae32cfd1b6e428
Bogdan Muravyov <googdddddd@gmail.com>
2023-10-12 18:43:57 +0300

    my-app

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,34 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use http\Env\Response;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Support\Facades\Hash;
+
 use App\Models\User;
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(StorePostRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required | string',
-            'email' => 'required | string | unique:users',
-            'password' => 'required | confirmed'
-    ]);
-
-        if ($validator->fails()) {
-            return Response(['errors' => $validator->errors()->all()], 422);
-        }
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
         ]);
 
-        if (!$user) {
-            return response()->json(['success' => false, "message" => "Registration failed"], 500);
-        }
-        return  response()->json(["success" => true, "message" => "Registration succeeded"], 500);
+        $user->createToken('Token Name')->accessToken;
+
+        return  response()->json(['success' => true, 'message' => 'Registration succeeded'], 201);
     }
 }
